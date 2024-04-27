c8a4380f44b96ce55ed356558c73162b4e615873
Loubna <loubna.alfidi@gmail.com>
2024-04-27 01:58:45 +0100

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -2,11 +2,9 @@
 """ Convert Markdown file to HTML file. """
 import sys
 import os
-import markdown
 
 
 def convert_markdown_to_html(markdown_file, output_file):
-    """ Convert Markdown file to HTML file. """
     # Check if the number of arguments is correct
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
@@ -21,12 +19,17 @@ def convert_markdown_to_html(markdown_file, output_file):
     with open(markdown_file, 'r') as f:
         markdown_content = f.read()
 
-    # Convert Markdown to HTML
-    html_content = markdown.markdown(markdown_content)
+    # Convert Markdown to HTML (simple implementation)
+    html_content = "<html><body>" + markdown_content + "</body></html>"
 
     # Write the HTML content to the output file
     with open(output_file, 'w') as f:
         f.write(html_content)
 
     # Print success message
+    print(f"Markdown content from {markdown_file} successfully converted to HTML and saved in {output_file}")
     sys.exit(0)
+
+
+if __name__ == "__main__":
+    convert_markdown_to_html(sys.argv[1], sys.argv[2])
